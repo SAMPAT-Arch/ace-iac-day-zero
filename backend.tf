1acee78dd677a463e3f4fc897a9763f641092952
Sampataceadmindevnetsecopsadmin <sllohar123@yahoo.co.in>
2024-10-28 14:25:47 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = "sampatarch2024"
+    organization = "aceincace"
     workspaces {
       name = "ace-iac-day-zero"
     }
